24c84bfa4c908ea629fdd23432b854a663b298bd
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2022-01-03 00:51:43 -0400

    light design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,9 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ]
 }
