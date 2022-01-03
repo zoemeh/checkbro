7ccece4727e60fc040404e1d075286a7988ed708
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2022-01-02 22:21:12 -0400

    essential gems

--- Gemfile
+++ Gemfile
@@ -44,10 +44,15 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Execute jobs in the background [https://github.com/mperham/sidekiq]
 gem "sidekiq", "~> 6.3"
+gem 'sorcery'
+gem "view_component"
+gem "slim-rails"
+gem 'simple_form'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "pry-rails"
 
   # Reduces boot times through caching; required in config/boot.rb
   gem "bootsnap", require: false
@@ -56,6 +61,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem 'solargraph'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   gem "rack-mini-profiler"
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_03_022047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at", precision: 6
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at", precision: 6
+    t.datetime "reset_password_email_sent_at", precision: 6
+    t.integer "access_count_to_reset_password_page", default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+  end
 
 end
