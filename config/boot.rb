cc7d5421e915ecee6d744b32dbf913a01f9b2420
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2022-01-02 23:35:32 -0400

    more init stuff

--- config/boot.rb
+++ config/boot.rb
@@ -3,5 +3,6 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 # Set up gems listed in the Gemfile.
 require "bundler/setup"
 
+ENV['DISABLE_BOOTSNAP_LOAD_PATH_CACHE'] = '1' # <---------- ADD THIS LINE
 # Speed up boot time by caching expensive operations, but not in production.
 require "bootsnap/setup" if Gem.loaded_specs.has_key?("bootsnap")
